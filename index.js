0302db6d2e7761e83fb8e74298467851b3af33ed
filipbosnjak <filipo.bosnjak@gmail.com>
2023-01-13 21:50:13 +0100

    polja, server

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("asdf");
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  console.log("pozvan /");
+  res.json({
+    josip: "josip",
+  });
+});
+
+app.listen(3000, () => {
+  console.log(typeof "");
+  let polje = [1, 2, 3, 4];
+  let polje2 = [];
+  polje.forEach((broj) => {
+    polje2.push(broj);
+  });
+  console.log("polje2: ", polje2);
+  console.log("123");
+  console.log("Server start");
+});
